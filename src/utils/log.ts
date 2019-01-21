6aeb3913457368881db0255afaa56c61e2974d73
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-01-21 12:33:45 -0500

    chore(log): ilp-logger instead of pino

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,14 +1,17 @@
+import createLogger from 'ilp-logger'
 import pino from 'pino'
 
-export default (name: string) =>
-  pino({
-    name,
-    level: 'trace',
-    base: {
-      name
-    },
-    prettyPrint: {
-      colorize: true,
-      translateTime: 'yyyy-mm-dd HH:MM:ss.l'
-    }
-  })
+// export default (name: string) =>
+//   pino({
+//     name,
+//     level: 'trace',
+//     base: {
+//       name
+//     },
+//     prettyPrint: {
+//       colorize: true,
+//       translateTime: 'yyyy-mm-dd HH:MM:ss.l'
+//     }
+//   })
+
+export default (name: string) => createLogger(name)
