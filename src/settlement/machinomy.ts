003dd9dd27ddea54cd3f88f09e771c0158309c89
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-02-19 08:31:22 -0500

    feat(machinomy): much more reliable balances, withdrawals working
    - queue in new eth plugin emits updates whenever channel state is updated (old version watching properties didn't work well)
    - simpler & easier to understand
    - miscellaneous fixes to get withdraw working

--- src/settlement/machinomy.ts
+++ src/settlement/machinomy.ts
@@ -1,7 +1,13 @@
-import { convert, eth, gwei, usd, wei } from '@kava-labs/crypto-rate-utils'
+import { convert, eth, gwei, wei } from '@kava-labs/crypto-rate-utils'
 import BigNumber from 'bignumber.js'
-import { isValidPrivate, privateToAddress, toBuffer } from 'ethereumjs-util'
-import EthereumPlugin, { AccountData } from 'ilp-plugin-ethereum'
+import { privateToAddress, toBuffer } from 'ethereumjs-util'
+import EthereumPlugin, {
+  EthereumAccount,
+  remainingInChannel,
+  spentFromChannel,
+  PaymentChannel,
+  ClaimablePaymentChannel
+} from 'ilp-plugin-ethereum'
 import {
   AuthorizeDeposit,
   AuthorizeWithdrawal,
@@ -11,16 +17,13 @@ import {
   distinctBigNum
 } from '../uplink'
 import { MemoryStore } from '../utils/store'
-import { streamMoney } from '../services/switch'
 import Web3 from 'web3'
 import { HttpProvider } from 'web3/providers'
 import createLogger from '../utils/log'
 import { SettlementEngine, SettlementEngineType } from '../engine'
 import { fetchGasPrice } from './shared/eth'
 import { LedgerEnv, State, SettlementModule } from '..'
-import { Option, some, none } from 'fp-ts/lib/Option'
-import { Brand } from '../types/util'
-import { Subject, BehaviorSubject } from 'rxjs'
+import { BehaviorSubject, fromEvent } from 'rxjs'
 import { map } from 'rxjs/operators'
 
 /**
@@ -79,21 +82,6 @@ export interface ValidatedEthereumPrivateKey {
   privateKey: string
 }
 
-// TODO Add this to setupCredential!
-// export const validate = (
-//   privateKey: string
-// ): Option<ValidatedEthereumPrivateKey> => {
-//   // Requires a 0x in front to validate private key, so prepend it if it's missing
-//   // (Web3 also throws an obscure error if not)
-
-//   return isValidPrivate(toBuffer(privateKey))
-//     ? some({
-//         settlerType: SettlementEngineType.Machinomy,
-//         privateKey
-//       })
-//     : none
-// }
-
 export type ReadyEthereumCredential = {
   settlerType: SettlementEngineType.Machinomy
   privateKey: string
@@ -135,7 +123,7 @@ export interface MachinomyUplinkConfig extends BaseUplinkConfig {
 export interface MachinomyBaseUplink extends BaseUplink {
   plugin: EthereumPlugin
   settlerType: SettlementEngineType.Machinomy
-  pluginAccount$: Subject<AccountData>
+  pluginAccount: EthereumAccount
 }
 
 export type ReadyMachinomyUplink = MachinomyBaseUplink & ReadyUplink
@@ -147,100 +135,71 @@ export const connectUplink = (credential: ReadyEthereumCredential) => (
   const store = config.plugin.store
 
   const { privateKey: ethereumPrivateKey } = credential
+
   const settler = state.settlers[credential.settlerType]
   const { ethereumProvider } = settler
 
   const getGasPrice = () => fetchGasPrice(state)(settler)
 
-  const pluginAccount$ = new Subject<AccountData>()
-  const storeProxy = new Proxy(store, {
-    set: (target, key, val) => {
-      if (key === 'account') {
-        pluginAccount$.next(JSON.parse(val))
-      }
-
-      return Reflect.set(target, key, val)
-    }
-  })
-
   const plugin = new EthereumPlugin(
     {
+      role: 'client',
       server,
       ethereumPrivateKey,
       ethereumProvider,
       getGasPrice
     },
     {
-      store: new MemoryStore(storeProxy),
+      store: new MemoryStore(store),
       log: createLogger('ilp-plugin-ethereum')
     }
   )
 
-  // TODO Updates to outgoingChannelCache may not trigger updates here, which is bad!
+  const pluginAccount = await plugin._loadAccount('peer')
+
+  const toEth = map<BigNumber, BigNumber>(amount => convert(wei(amount), eth()))
 
   const totalSent$ = new BehaviorSubject(new BigNumber(0))
-  pluginAccount$
+  fromEvent<PaymentChannel | undefined>(pluginAccount.account.outgoing, 'data')
     .pipe(
-      map(
-        account =>
-          new BigNumber(
-            account.bestOutgoingClaim ? account.bestOutgoingClaim.value : 0
-          )
-      ),
+      map(spentFromChannel),
       distinctBigNum,
-      map(value => convert(wei(value), eth()))
+      toEth
     )
     .subscribe(totalSent$)
 
   const outgoingCapacity$ = new BehaviorSubject(new BigNumber(0))
-  pluginAccount$
+  fromEvent<PaymentChannel | undefined>(pluginAccount.account.outgoing, 'data')
     .pipe(
-      map(account =>
-        account.outgoingChannelId
-          ? plugin.channelCache[account.outgoingChannelId]
-            ? plugin.channelCache[account.outgoingChannelId].value.minus(
-                account.bestOutgoingClaim
-                  ? account.bestOutgoingClaim.value
-                  : new BigNumber(0)
-              )
-            : new BigNumber(0)
-          : new BigNumber(0)
-      ),
+      map(remainingInChannel),
       distinctBigNum,
-      map(value => convert(wei(value), eth()))
+      toEth
     )
     .subscribe(outgoingCapacity$)
 
   const totalReceived$ = new BehaviorSubject(new BigNumber(0))
-  pluginAccount$
+  fromEvent<ClaimablePaymentChannel | undefined>(
+    pluginAccount.account.incoming,
+    'data'
+  )
     .pipe(
-      map(
-        account =>
-          new BigNumber(
-            account.bestIncomingClaim ? account.bestIncomingClaim.value : 0
-          )
-      ),
+      map(spentFromChannel),
       distinctBigNum,
-      map(value => convert(wei(value), eth()))
+      toEth
     )
     .subscribe(totalReceived$)
 
   const incomingCapacity$ = new BehaviorSubject(new BigNumber(0))
-  pluginAccount$
+  fromEvent<ClaimablePaymentChannel | undefined>(
+    pluginAccount.account.incoming,
+    'data'
+  )
     .pipe(
-      map(account =>
-        account.bestIncomingClaim
-          ? plugin.channelCache[account.bestIncomingClaim.channelId]
-            ? plugin.channelCache[
-                account.bestIncomingClaim.channelId
-              ].value.minus(account.bestIncomingClaim.value)
-            : new BigNumber(0)
-          : new BigNumber(0)
-      ),
+      map(remainingInChannel),
       distinctBigNum,
-      map(value => convert(wei(value), eth()))
+      toEth
     )
-    .subscribe(outgoingCapacity$)
+    .subscribe(incomingCapacity$)
 
   return {
     settlerType: SettlementEngineType.Machinomy,
@@ -249,7 +208,7 @@ export const connectUplink = (credential: ReadyEthereumCredential) => (
     incomingCapacity$,
     totalReceived$,
     totalSent$,
-    pluginAccount$,
+    pluginAccount,
     plugin
   }
 }
@@ -273,9 +232,8 @@ export const deposit = (uplink: ReadyMachinomyUplink) => () => async ({
   amount: BigNumber
   authorize: AuthorizeDeposit
 }) => {
-  const fundAmountWei = convert(eth(amount), wei())
-
-  await uplink.plugin.fundOutgoingChannel(fundAmountWei, async fee => {
+  const fundAmountGwei = convert(eth(amount), gwei())
+  await uplink.pluginAccount.fundOutgoingChannel(fundAmountGwei, async fee => {
     // TODO Check the base layer balance to confirm there's enough $$$ on chain (with fee)!
 
     await authorize({
@@ -284,32 +242,29 @@ export const deposit = (uplink: ReadyMachinomyUplink) => () => async ({
     })
   })
 
-  // TODO This is a hack to get the balance to update -- fix this
-  uplink.pluginAccount$.next(uplink.plugin.account)
+  // Inform the peer so they can link the channel
+  await uplink.plugin.sendMoney('0')
 
   // TODO Add functionality to request/get incoming capacity!
-  // TODO Watch "availableToReceive" -- when that becomes > 0
-  //      (subject to some timeout, of course)
 }
 
 const withdraw = (uplink: ReadyMachinomyUplink) => (state: State) => async (
   authorize: AuthorizeWithdrawal
 ) => {
-  const claimChannel = uplink.plugin.claimIfProfitable(false, async fee => {
-    await authorize({
-      // TODO plugin itself should return the value so we know EXACTLY how much will be claimed
-      value: uplink.outgoingCapacity$.value.plus(uplink.totalReceived$.value),
-      fee: convert(wei(fee), eth())
-    })
-  })
+  const claimChannel = uplink.pluginAccount.claimIfProfitable(
+    false,
+    async (channel, fee) => {
+      await authorize({
+        value: uplink.outgoingCapacity$.value.plus(channel.spent),
+        fee: convert(wei(fee), eth())
+      })
+    }
+  )
 
-  const requestClose = uplink.plugin._requestClose()
+  const requestClose = uplink.pluginAccount.requestClose()
 
   // Simultaneously withdraw and request incoming capacity to be removed
   await Promise.all([claimChannel, requestClose])
-
-  // TODO This is a ahck to get the balance to updated
-  uplink.pluginAccount$.next(uplink.plugin.account)
 
   // TODO Also, confirm the incoming capacity has been closed -- or attempt to dispute it?
 }
