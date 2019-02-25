dde48521e4e7272634d47bda22d52a61abf13142
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2019-02-25 15:11:27 -0500

    improve input validation tests

--- src/__tests__/add-remove.test.ts
+++ src/__tests__/add-remove.test.ts
@@ -7,7 +7,7 @@ import {
   SettlementEngineType,
   ReadyUplinks
 } from '..'
-import { addXrp, addEth, addBtc, testExchange } from './_helpers'
+import { addXrp, addEth, addBtc } from './_helpers'
 
 const test = anyTest as TestInterface<SwitchApi>
 
@@ -46,31 +46,60 @@ test('cannot add duplicate btc uplink', async t => {
 })
 
 // Test credential config input validation
-// TODO expand input validation tests
-test('add with nonsense xrp secret throws', async t => {
+// Private key and credential validation is done by lower level libraries.
+
+test('add with invalid xrp secret throws', async t => {
   await t.throwsAsync(
     t.context.add({
       settlerType: SettlementEngineType.XrpPaychan,
-      secret: 'this is not an xrp secret'
-    })
+      secret: 'this is not a valid xrpSecret' // invalid but correct length
+    }),
+    'Non-base58 character'
   )
 })
-test('add with nonsense eth secret throws', async t => {
+test('add with un-activated xrp secret throws', async t => {
+  await t.throwsAsync(
+    t.context.add({
+      settlerType: SettlementEngineType.XrpPaychan,
+      secret: 'sn5s78zYX1i9mzFmd8jXooDFYgfj2' // un-activated but valid secret
+    }),
+    'actNotFound'
+  )
+})
+// Test eth private keys. As long as they contain correct characters and are the right length they are a valid key.
+test('add with invalid eth secret throws', async t => {
   await t.throwsAsync(
     t.context.add({
       settlerType: SettlementEngineType.Machinomy,
-      privateKey: 'this is not an eth secret'
+      privateKey:
+        'this is not a valid eth secret despite being the correct leength'
     })
   )
+  // Note: if the secret is correct length but contains invalid characters, an invalid length error is thrown ('private key length is invalid').
 })
-test('add with nonsense lnd credentials throws', async t => {
+// Test valid lnd uri, but invalid credentials.
+test('add with invalid lnd credentials throws', async t => {
+  await t.throwsAsync(
+    t.context.add({
+      settlerType: SettlementEngineType.Lnd,
+      hostname: process.env[`LIGHTNING_LND_HOST_CLIENT_1`]!,
+      grpcPort: parseInt(process.env[`LIGHTNING_LND_GRPCPORT_CLIENT_1`]!, 10),
+      tlsCert:
+        'LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUNpRENDQWkrZ0F3SUJBZ0lRZG81djBRQlhIbmppNGhSYWVlTWpOREFLQmdncWhrak9QUVFEQWpCSE1SOHcKSFFZRFZRUUtFeFpzYm1RZ1lYVjBiMmRsYm1WeVlYUmxaQ0JqWlhKME1TUXdJZ1lEVlFRREV4dEtkWE4wZFhOegpMVTFoWTBKdmIyc3RVSEp2TFRNdWJHOWpZV3d3SGhjTk1UZ3dPREl6TURVMU9ERXdXaGNOTVRreE1ERTRNRFUxCk9ERXdXakJITVI4d0hRWURWUVFLRXhac2JtUWdZWFYwYjJkbGJtVnlZWFJsWkNCalpYSjBNU1F3SWdZRFZRUUQKRXh0S2RYTjBkWE56TFUxaFkwSnZiMnN0VUhKdkxUTXViRzlqWVd3d1dUQVRCZ2NxaGtqT1BRSUJCZ2dxaGtpTwpQUU1CQndOQ0FBU0ZoUm0rdy9UMTBQb0t0ZzRsbTloQk5KakpENDczZmt6SHdQVUZ3eTkxdlRyUVNmNzU0M2oyCkpyZ0ZvOG1iVFYwVnRwZ3FrZksxSU1WS01MckYyMXhpbzRIOE1JSDVNQTRHQTFVZER3RUIvd1FFQXdJQ3BEQVAKQmdOVkhSTUJBZjhFQlRBREFRSC9NSUhWQmdOVkhSRUVnYzB3Z2NxQ0cwcDFjM1IxYzNNdFRXRmpRbTl2YXkxUQpjbTh0TXk1c2IyTmhiSUlKYkc5allXeG9iM04wZ2dSMWJtbDRnZ3AxYm1sNGNHRmphMlYwaHdSL0FBQUJoeEFBCkFBQUFBQUFBQUFBQUFBQUFBQUFCaHhEK2dBQUFBQUFBQUFBQUFBQUFBQUFCaHhEK2dBQUFBQUFBQUF3bGM5WmMKazdiRGh3VEFxQUVFaHhEK2dBQUFBQUFBQUJpTnAvLytHeFhHaHhEK2dBQUFBQUFBQUtXSjV0bGlET1JqaHdRSwpEd0FDaHhEK2dBQUFBQUFBQUc2V3ovLyszYXRGaHhEOTJ0RFF5djRUQVFBQUFBQUFBQkFBTUFvR0NDcUdTTTQ5CkJBTUNBMGNBTUVRQ0lBOU85eHRhem1keENLajBNZmJGSFZCcTVJN0pNbk9GUHB3UlBKWFFmcllhQWlCZDVOeUoKUUN3bFN4NUVDblBPSDVzUnB2MjZUOGFVY1hibXlueDlDb0R1ZkE9PQotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0tCg==',
+      macaroon:
+        'AgEDbG5kArsBAwoQ3/I9f6kgSE6aUPd85lWpOBIBMBoWCgdhZGRyZXNzEgRyZWFkEgV3cml0ZRoTCgRpbmZvEgRyZWFkEgV32ml0ZRoXCghpbnZvaWNlcxIEcmVhZBIFd3JpdGUaFgoHbWVzc2FnZRIEcmVhZBIFd3JpdGUaFwoIb2ZmY2hhaW4SBHJlYWQSBXdyaXRlGhYKB29uY2hhaW4SBHJlYWQSBXdyaXRlGhQKBXBlZXJzEgRyZWFkEgV3cml0ZQAABiAiUTBv3Eh6iDbdjmXCfNxp4HBEcOYNzXhrm+ncLHf5jA=='
+    }),
+    'Failed to connect before the deadline'
+  )
+})
+test('add with invalid lnd uri throws', async t => {
   await t.throwsAsync(
     t.context.add({
       settlerType: SettlementEngineType.Lnd,
       hostname: 'nonsense',
+      grpcPort: 2000,
       tlsCert: 'nonsense',
-      macaroon: 'nonsense',
-      grpcPort: 12345
+      macaroon: 'nonsense'
     })
   )
 })
