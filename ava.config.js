4345eda7f4eda3840230c6387ee9f02012af5cb2
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-03-01 14:02:25 -0500

    test: try to fix codecov, again

--- ava.config.js
+++ ava.config.js
@@ -1,8 +1,8 @@
 export default {
-  files: ['build/__tests__/**/*.js'],
+  files: ['build/__tests__/**/*.test.js'],
   failFast: true,
   verbose: true,
   serial: true,
-  timeout: '3m',
+  timeout: '30s',
   require: ['source-map-support/register']
 }
