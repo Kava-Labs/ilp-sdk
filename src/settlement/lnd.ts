8e66f5c497dad423bcbb177a7480e9a1610c0df7
Kevin Davis <kjydavis3@gmail.com>
2019-03-17 13:42:26 -0400

    fix: lint

--- src/settlement/lnd.ts
+++ src/settlement/lnd.ts
@@ -222,9 +222,7 @@ const connectUplink = (credential: ReadyLndCredential) => (
   }
 }
 
-export const getBaseBalance = async (
-  credential: ReadyLndCredential
-) => {
+export const getBaseBalance = async (credential: ReadyLndCredential) => {
   const lndService = credential.service
   const baseBalance = await lndService.walletBalance(new WalletBalanceRequest())
   return new BigNumber(baseBalance.getConfirmedBalance())
