bbb5b3748a231c3c8f472c41874e5aa14a32179d
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2019-04-16 11:31:18 -0400

    test: persists state for multiple envs

--- src/__tests__/persistence.test.ts
+++ src/__tests__/persistence.test.ts
@@ -105,68 +105,41 @@ test('fails to reload encrypted state if no password is provided', async t => {
   await t.throwsAsync(() => connect(process.env.LEDGER_ENV as LedgerEnv))
 })
 
-// TODO How to test whether v0.3+ configs are upgraded to the new configs?
+test('persists state for multiple environments', async t => {
+  // Delete any existing config
+  await promisify(unlink)(CONFIG_PATH).catch(() => Promise.resolve())
 
-// test('supports loading states from v0.3.x or v0.4.x', async t => {
-//   // Delete any existing config
-//   await promisify(unlink)(CONFIG_PATH).catch(() => Promise.resolve())
+  // Connect API for testnet
+  const api = await connect(LedgerEnv.Testnet)
+  await Promise.all(
+    [addBtc, addXrp, addEth].map(createUplink => createUplink()(api))
+  )
+  await api.disconnect() // Should persist the state
 
-//   // Connect API
-//   const api = await connect(
-//     process.env.LEDGER_ENV as LedgerEnv,
-//     'some password'
-//   )
-//   await Promise.all(
-//     [addBtc, addXrp, addEth].map(createUplink => createUplink()(api))
-//   )
-//   await api.disconnect() // Should persist the state
+  // Connect API on mainnet
+  const api2 = await connect(LedgerEnv.Mainnet)
+  await Promise.all([addEth].map(createUplink => createUplink()(api2)))
+  await api2.disconnect() // Should persist the state
 
-//   await t.throwsAsync(() => connect(process.env.LEDGER_ENV as LedgerEnv))
-// })
+  const initialSerializedConfig = await readConfig()
 
-// TODO How to test support for multiple environments?
+  // Reconnect the API on mainnet
+  const api3 = await connect(LedgerEnv.Mainnet)
+  t.is(api3.state.credentials.length, 1, 'same number of credentials')
+  t.is(api3.state.uplinks.length, 1, 'same number of uplink')
+  await api3.disconnect()
 
-// test('persists state for multiple environments', async t => {
-//   // Delete any existing config
-//   await promisify(unlink)(CONFIG_PATH).catch(() => Promise.resolve())
+  // Reconnect the API on testnet
+  const api4 = await connect(LedgerEnv.Testnet)
+  t.is(api4.state.credentials.length, 3, 'same number of credentials')
+  t.is(api4.state.uplinks.length, 3, 'same number of uplink')
+  await api4.disconnect()
 
-//   // Start running a local connector
-//   await promisify(start)('connector-config/ecosystem.config.js')
+  const rebuiltSerializedConfig = await readConfig()
 
-//   // Connect API for a local connector
-//   const api = await connect(LedgerEnv.Local)
-//   await Promise.all(
-//     [addBtc, addXrp, addEth].map(createUplink => createUplink()(api))
-//   )
-//   await api.disconnect() // Should persist the state
-
-//   // Stop local connector
-//   await promisify(start)('connector-config/ecosystem.config.js')
-
-//   // Connect API on testnet
-//   const api2 = await connect(LedgerEnv.Testnet)
-//   await Promise.all([addBtc, addEth].map(createUplink => createUplink()(api2)))
-//   await api2.disconnect() // Should persist the state
-
-//   const initialSerializedConfig = await readConfig()
-
-//   // Reconnect the API on testnet
-//   const api3 = await connect(LedgerEnv.Testnet)
-//   t.is(api3.state.credentials.length, 3, 'same number of credentials')
-//   t.is(api3.state.uplinks.length, 3, 'same number of uplink')
-//   await api3.disconnect()
-
-//   // Reconnect the API a local connector
-//   const api4 = await connect(LedgerEnv.Local)
-//   t.is(api4.state.credentials.length, 2, 'same number of credentials')
-//   t.is(api4.state.uplinks.length, 2, 'same number of uplink')
-//   await api4.disconnect()
-
-//   const rebuiltSerializedConfig = await readConfig()
-
-//   t.is(
-//     rebuiltSerializedConfig,
-//     initialSerializedConfig,
-//     'config is persisted and can be rebuilt from the persisted version'
-//   )
-// })
+  t.is(
+    rebuiltSerializedConfig,
+    initialSerializedConfig,
+    'config is persisted and can be rebuilt from the persisted version'
+  )
+})
